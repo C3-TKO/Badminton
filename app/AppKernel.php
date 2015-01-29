b5ec5adb83a15f907ec889200467c11ae825244a
Thomas Kolar <thomas.kolar@email.de>
2015-01-29 11:55:16 +0100

    Changes default caching / Adds empty Badminton\StatisticsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Badminton\StatisticsBundle\StatisticsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -32,5 +33,15 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    public function getCacheDir()
+    {
+        return '/tmp/symfony/cache/'. $this->environment;
+    }
+
+    public function getLogDir()
+    {
+        return '/tmp/symfony/log/'. $this->environment;
     }
 }
---
+++ src/Badminton/StatisticsBundle/StatisticsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Badminton\StatisticsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StatisticsBundle extends Bundle
+{
+}
