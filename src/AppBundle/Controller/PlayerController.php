6f86c99612b1be07d1f11caa2f65f7d8193b0617
Thomas Kolar <thomas.kolar@email.de>
2015-01-31 12:16:53 +0100

    Adds crud for player entity

---
+++ src/AppBundle/Controller/PlayerController.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use AppBundle\Entity\Player;
+use AppBundle\Form\PlayerType;
+
+/**
+ * Player controller.
+ *
+ */
+class PlayerController extends Controller
+{
+
+    /**
+     * Lists all Player entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AppBundle:Player')->findAll();
+
+        return $this->render('AppBundle:Player:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Player entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Player();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('crud_player__show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('AppBundle:Player:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Player entity.
+     *
+     * @param Player $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Player $entity)
+    {
+        $form = $this->createForm(new PlayerType(), $entity, array(
+            'action' => $this->generateUrl('crud_player__create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Player entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Player();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('AppBundle:Player:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Player entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Player')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Player entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AppBundle:Player:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Player entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Player')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Player entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AppBundle:Player:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Player entity.
+    *
+    * @param Player $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Player $entity)
+    {
+        $form = $this->createForm(new PlayerType(), $entity, array(
+            'action' => $this->generateUrl('crud_player__update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Player entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Player')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Player entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('crud_player__edit', array('id' => $id)));
+        }
+
+        return $this->render('AppBundle:Player:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Player entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AppBundle:Player')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Player entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('crud_player_'));
+    }
+
+    /**
+     * Creates a form to delete a Player entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('crud_player__delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
