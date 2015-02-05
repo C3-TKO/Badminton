20e21afad8b1125b193aa0901c8a8d9c7e1ca70b
Thomas Kolar <thomas.kolar@email.de>
2015-02-05 17:32:12 +0100

    Adds relation between players and team

--- src/AppBundle/Entity/Player.php
+++ src/AppBundle/Entity/Player.php
@@ -52,4 +52,115 @@ class Player
     {
         return $this->id;
     }
+    /**
+     * @var \AppBundle\Entity\Team
+     */
+    private $team_a;
+
+
+    /**
+     * Set team_a
+     *
+     * @param \AppBundle\Entity\Team $teamA
+     * @return Player
+     */
+    public function setTeamA(\AppBundle\Entity\Team $teamA = null)
+    {
+        $this->team_a = $teamA;
+
+        return $this;
+    }
+
+    /**
+     * Get team_a
+     *
+     * @return \AppBundle\Entity\Team 
+     */
+    public function getTeamA()
+    {
+        return $this->team_a;
+    }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $team_player_a;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->team_player_a = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add team_player_a
+     *
+     * @param \AppBundle\Entity\Team $teamPlayerA
+     * @return Player
+     */
+    public function addTeamPlayerA(\AppBundle\Entity\Team $teamPlayerA)
+    {
+        $this->team_player_a[] = $teamPlayerA;
+
+        return $this;
+    }
+
+    /**
+     * Remove team_player_a
+     *
+     * @param \AppBundle\Entity\Team $teamPlayerA
+     */
+    public function removeTeamPlayerA(\AppBundle\Entity\Team $teamPlayerA)
+    {
+        $this->team_player_a->removeElement($teamPlayerA);
+    }
+
+    /**
+     * Get team_player_a
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getTeamPlayerA()
+    {
+        return $this->team_player_a;
+    }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $team_player_b;
+
+
+    /**
+     * Add team_player_b
+     *
+     * @param \AppBundle\Entity\Team $teamPlayerB
+     * @return Player
+     */
+    public function addTeamPlayerB(\AppBundle\Entity\Team $teamPlayerB)
+    {
+        $this->team_player_b[] = $teamPlayerB;
+
+        return $this;
+    }
+
+    /**
+     * Remove team_player_b
+     *
+     * @param \AppBundle\Entity\Team $teamPlayerB
+     */
+    public function removeTeamPlayerB(\AppBundle\Entity\Team $teamPlayerB)
+    {
+        $this->team_player_b->removeElement($teamPlayerB);
+    }
+
+    /**
+     * Get team_player_b
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getTeamPlayerB()
+    {
+        return $this->team_player_b;
+    }
 }
--- src/AppBundle/Entity/Team.php
+++ src/AppBundle/Entity/Team.php
@@ -108,4 +108,105 @@ class Team
     {
         return $this->id;
     }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $players_a;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->players_a = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add players_a
+     *
+     * @param \AppBundle\Entity\Player $playersA
+     * @return Team
+     */
+    public function addPlayersA(\AppBundle\Entity\Player $playersA)
+    {
+        $this->players_a[] = $playersA;
+
+        return $this;
+    }
+
+    /**
+     * Remove players_a
+     *
+     * @param \AppBundle\Entity\Player $playersA
+     */
+    public function removePlayersA(\AppBundle\Entity\Player $playersA)
+    {
+        $this->players_a->removeElement($playersA);
+    }
+
+    /**
+     * Get players_a
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getPlayersA()
+    {
+        return $this->players_a;
+    }
+    /**
+     * @var \AppBundle\Entity\Player
+     */
+    private $teams_a;
+
+
+    /**
+     * Set teams_a
+     *
+     * @param \AppBundle\Entity\Player $teamsA
+     * @return Team
+     */
+    public function setTeamsA(\AppBundle\Entity\Player $teamsA = null)
+    {
+        $this->teams_a = $teamsA;
+
+        return $this;
+    }
+
+    /**
+     * Get teams_a
+     *
+     * @return \AppBundle\Entity\Player 
+     */
+    public function getTeamsA()
+    {
+        return $this->teams_a;
+    }
+    /**
+     * @var \AppBundle\Entity\Player
+     */
+    private $teams_b;
+
+
+    /**
+     * Set teams_b
+     *
+     * @param \AppBundle\Entity\Player $teamsB
+     * @return Team
+     */
+    public function setTeamsB(\AppBundle\Entity\Player $teamsB = null)
+    {
+        $this->teams_b = $teamsB;
+
+        return $this;
+    }
+
+    /**
+     * Get teams_b
+     *
+     * @return \AppBundle\Entity\Player 
+     */
+    public function getTeamsB()
+    {
+        return $this->teams_b;
+    }
 }
