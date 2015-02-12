7f2d9124918456c2ffecf5c7ce34ea2ef192917c
Thomas Kolar <thomas.kolar@email.de>
2015-02-12 01:38:53 +0100

    Adds ranking per round service stub

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        var_dump( $this->get('app.round_ranking')->findByRoundId(1) );
         return $this->render('AppBundle:Default:index.html.twig', $this->get('app.round_results')->findByLastRound() );
     }
 }
