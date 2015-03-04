abbf18644a171fdb5c9db8e692bce929b3fcdcdf
Thomas Kolar <thomas.kolar@email.de>
2015-03-04 20:19:59 +0100

    Adds method for generating all combination of k elements out of set n

--- src/AppBundle/Services/Matchscheduling/MatchScheduler.php
+++ src/AppBundle/Services/Matchscheduling/MatchScheduler.php
@@ -6,15 +6,17 @@ use AppBundle\Entity\Player;
 use Doctrine\Common\Collections\ArrayCollection;
 
 /**
- * Class RoundResults
- * @package AppBundle\Services
+ * Class MatchScheduler
+ * @package AppBundle\Services\MatchScheduling
  */
 class MatchScheduler
 {
+    const MAX_NUMBER_PLAYERS_PLAYING = 4;
+
     /**
      * @var ArrayCollection Player[]
      */
-    private $playerList = array();
+    private $playerList = null;
 
     public function setPlayerList( ArrayCollection $playerList ) {
         $this->playerList = $playerList;
@@ -22,13 +24,11 @@ class MatchScheduler
 
     public function schedule()
     {
-        if ($this->playerList->count() !== 4) {
-            throw new \Exception('Aktuell kann nur für 4 Spieler ein Spielplan erstellt werden!');
-        }
+        $this->getAllPlayerCombinations(array('A','B','C','D','E'), self::MAX_NUMBER_PLAYERS_PLAYING);
 
         $test = $this->assign4PlayerMatches($this->playerList->get(0), $this->playerList->get(1), $this->playerList->get(2), $this->playerList->get(3));
 
-        /*
+
         foreach($test as $match) {
             $player1 = array_shift($match);
             $player2 = array_shift($match);
@@ -36,7 +36,6 @@ class MatchScheduler
             $player4 = array_shift($match);
             echo $player1->getName(). ' & ' .$player2->getName(). ' vs. '.$player3->getName(). ' & '. $player4->getName().'<br/>';
         }
-        */
     }
 
     /**
@@ -53,8 +52,6 @@ class MatchScheduler
      * @return array
      */
     private function assign4PlayerMatches(Player $player1, Player $player2, Player $player3, Player $player4) {
-
-
         $shuffledPlayers = array($player1, $player2, $player3, $player4);
         shuffle($shuffledPlayers);
 
@@ -71,5 +68,45 @@ class MatchScheduler
         );
 
         return $result;
+    }
+
+    /**
+     * Retrieves all player combinations for match setups
+     *
+     * @param array     $n  Complete set of all available players
+     * @param integer   $k
+     */
+    private function getAllPlayerCombinations($n, $k) {
+
+        $result = array();
+        for($i = 0; $i < $k; $i++) {
+            $result[] = null;
+        }
+
+        $this->returnAllCombinationsOfKElementsFromN($n, $k, 0, $result);
+
+
+    }
+
+
+    /**
+     * Returns all unordered and unique combinations of k elements out of a set of n completely available elements.
+     *
+     * @param array     $n          All available elements
+     * @param integer   $k          Number of desired elements to be combined
+     * @param integer   $draw       This is the number of draws already performed
+     * @param array     $drawnKs    Contains the already drawn elements (k)
+     */
+    private function returnAllCombinationsOfKElementsFromN($n, $k, $draw, $drawnKs) {
+        if($k === 0) {
+            var_dump($drawnKs);
+            echo '<br/>';
+            return;
+        }
+        for ($i = $draw; $i <= (count($n) - $k); $i++ ) {
+            $countDrawnKs = count($drawnKs);
+            $drawnKs[$countDrawnKs-$k] = $n[$i];
+            $this->returnAllCombinationsOfKElementsFromN($n, $k-1, $i+1, $drawnKs);
+        }
     }
 }
