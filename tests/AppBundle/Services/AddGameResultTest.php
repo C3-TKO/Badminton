8c5ee3260a4a38d0ae851e5a4e8e2c263e20d160
Thomas Kolar <thomas.kolar@email.de>
2016-03-14 22:43:01 +0100

    Changes the phpunit.xml.dist and phpunit.xml

---
+++ tests/AppBundle/Services/AddGameResultTest.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of the Badminton AppBundle (https://github.com/C3-TKO/Badminton).
+ *
+ * (c) Thomas Kolar <thomas.kolar@email.de>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+
+namespace Tests\AppBundle\Services;
+
+
+class AddGameResultTest {
+
+}
