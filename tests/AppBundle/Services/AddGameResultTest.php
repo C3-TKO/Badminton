e0fec760a52bb54f4baa2b61e5cfdef9b6711b36
Thomas Kolar <thomas.kolar@email.de>
2016-03-16 21:19:56 +0100

    Adds the unit test for the addGame service

--- tests/AppBundle/Services/AddGameResultTest.php
+++ tests/AppBundle/Services/AddGameResultTest.php
@@ -10,6 +10,8 @@
 
 
 namespace Tests\AppBundle\Services;
+use AppBundle\Entity\Game;
+use AppBundle\Entity\Team;
 
 /**
  * Class AddGameResultTest
@@ -18,11 +20,68 @@ namespace Tests\AppBundle\Services;
 class AddGameResultTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * Tests the main method of the add game service
+     *
      * @test
      * @small
+     *
+     * @dataProvider addGameProvider
+     *
+     * @param \AppBundle\Entity\Game $game
+     * @param \AppBundle\Entity\Team $winningTeam
+     * @param $scoreLoosingTeam
+     * @param \AppBundle\Entity\Game $expectation
      */
-    public function simple()
+    public function addGame(Game $game, Team $winningTeam, $scoreLoosingTeam, Game $expectation)
     {
-        $this->assertTrue(true);
+        $this->assertEquals($expectation, $game);
+    }
+
+    /**
+     * DataProvider for the addGame test
+     */
+    public function addGameProvider()
+    {
+        $game            = new Game();
+        $teamA           = new Team();
+        $teamB           = new Team();
+        $reflectionClass = new \ReflectionClass('\AppBundle\Entity\Team');
+
+        $reflectionProperty = $reflectionClass->getProperty('id');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($teamA, 1);
+        $reflectionProperty->setValue($teamB, 2);
+
+        $game->setTeamA($teamA);
+        $game->setTeamB($teamB);
+
+        $expectedGame = clone $game;
+
+        return [
+            [
+                'game'              => $game,
+                'winningTeam'       => $teamA,
+                'scoreLoosingTeam'  => 0,
+                'expectation'       => $expectedGame->setTeamAScore(21)->setTeamBScore(0)
+            ],
+            [
+                'game'              => $game,
+                'winningTeam'       => $teamA,
+                'scoreLoosingTeam'  => 19,
+                'expectation'       => $expectedGame->setTeamAScore(21)->setTeamBScore(0)
+            ],
+            [
+                'game'              => $game,
+                'winningTeam'       => $teamA,
+                'scoreLoosingTeam'  => 20,
+                'expectation'       => $expectedGame->setTeamAScore(22)->setTeamBScore(0)
+            ],
+            [
+                'game'              => $game,
+                'winningTeam'       => $teamA,
+                'scoreLoosingTeam'  => 29,
+                'expectation'       => $expectedGame->setTeamAScore(30)->setTeamBScore(0)
+            ]
+        ];
     }
 }
